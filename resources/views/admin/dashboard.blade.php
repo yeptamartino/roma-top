e01ee17b7aed91e3f996d638543300d525dcbd92
yeptamartino <yeptamartino63@gmail.com>
2021-02-24 13:46:43 +0700

    fixed button history

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -56,6 +56,77 @@ Dashboard
     </div>
     <div class="box">
       <div class="box-header with-border">
+        <h3 class="box-title"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#historyTransaksi">Tampilkan History Transaksi</button></h3>
+      </div>
+        <div class="row">
+          <div class="modal fade" id="historyTransaksi">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                  <h4 class="modal-title">History Transaksi</h4>
+                </div>
+                <div class="modal-body table-responsive">
+                  <table id="history" class="table">
+                    <thead>
+                      <tr>
+                        <th>No.</th>
+                        {{-- <th>Tgl.</th> --}}
+                        <th>Nama Pelanggan.</th>
+                        {{-- <th>Total Transaksi</th> --}}
+                        {{-- <th>Total Bayar</th> --}}
+                        <th>Item Transaksi</th>
+                        <th>Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @if(count($transactions) > 0)
+                      @foreach($transactions as $transaction)
+                      <tr>
+                        <td>{{ $transaction->id }}</td>
+                        {{-- <td>{{ $transaction->created_at->format('d, M Y') }}</td> --}}
+                        @if($transaction->customer)
+                          <td>{{ $transaction->customer->name }}</td>
+                        @else
+                          <td>-</td>
+                        @endif
+                        {{-- <td>@rupiah($transaction->total_price())</td> --}}
+                        {{-- <td>@rupiah($transaction->total_paid)</td> --}}
+                        <td>
+                          <ul>
+                            @foreach($transaction->transaction_items as $transaction_item)
+                              <li>{{ $transaction_item->name }} (@rupiah($transaction_item->selling_price))</li>
+                            @endforeach
+                          </ul>
+                        </td>
+                        <td>
+                            <a
+                            href="{{ route('admin.sales.detail', ['id' => $transaction->id]) }}"
+                            class="btn btn-primary"
+                            >
+                            <i class="fa fa-eye"></i>
+                          </a>
+                        </td>
+                  </tr>
+                  @endforeach
+                  @else
+                    <tr>
+                      <td colspan="4">
+                      Belum Ada Data.
+                      </td>
+                    </tr>
+                  @endif
+                </tbody>
+                </table>    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="box">
+      <div class="box-header with-border">
         <h3 class="box-title">Daftar Katalog</h3>
         <div class="box-tools pull-right">
           <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
@@ -107,86 +178,6 @@ Dashboard
         </div>
       </div>
     </div>
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">History Transaksi</h3>
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i>
-            </button>
-          </div>
-        </div>
-        <div class="box-body">
-          <div class="row">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#historyTransaksi">Tampilkan History Transaksi</button>
-            <div class="modal fade" id="historyTransaksi">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">×</span></button>
-                    <h4 class="modal-title">History Transaksi</h4>
-                  </div>
-                  <div class="modal-body table-responsive">
-                    <table id="history" class="table">
-                      <thead>
-                        <tr>
-                          <th>No.</th>
-                          {{-- <th>Tgl.</th> --}}
-                          <th>Nama Pelanggan.</th>
-                          {{-- <th>Total Transaksi</th> --}}
-                          {{-- <th>Total Bayar</th> --}}
-                          <th>Item Transaksi</th>
-                          <th>Aksi</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @if(count($transactions) > 0)
-                        @foreach($transactions as $transaction)
-                        <tr>
-                          <td>{{ $transaction->id }}</td>
-                          {{-- <td>{{ $transaction->created_at->format('d, M Y') }}</td> --}}
-                          @if($transaction->customer)
-                            <td>{{ $transaction->customer->name }}</td>
-                          @else
-                            <td>-</td>
-                          @endif
-                          {{-- <td>@rupiah($transaction->total_price())</td> --}}
-                          {{-- <td>@rupiah($transaction->total_paid)</td> --}}
-                          <td>
-                            <ul>
-                              @foreach($transaction->transaction_items as $transaction_item)
-                                <li>{{ $transaction_item->name }} ({{$transaction_item->quantity}} x @rupiah($transaction_item->selling_price))</li>
-                              @endforeach
-                            </ul>
-                          </td>
-                          <td>
-                              <a
-                              href="{{ route('admin.sales.detail', ['id' => $transaction->id]) }}"
-                              class="btn btn-primary"
-                              >
-                              <i class="fa fa-eye"></i>
-                            </a>
-                          </td>
-                    </tr>
-                    @endforeach
-                    @else
-                      <tr>
-                        <td colspan="4">
-                        Belum Ada Data.
-                        </td>
-                      </tr>
-                    @endif
-                  </tbody>
-                  </table>    
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
   @endsection
 
